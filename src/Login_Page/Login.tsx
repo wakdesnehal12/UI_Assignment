493bd32786e073fdd2bbd2586ea08d61bab5062a
SnehalWakde <snehal.wakde@extwebtech.in>
2022-05-10 14:13:06 +0530

    first commit

---
+++ src/Login_Page/Login.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Box, Button, Container, Typography } from '@material-ui/core';
+import login_banner from '../assets/login-banner.png';
+import logo from '../assets/logo.png';
+import flag from '../assets/Flag_of_India.png';
+import phone_icon from '../assets/phone_icon.png';
+export default function Login() {
+
+return (
+    <div>
+        <Container className='mainBox'>
+            <Grid container>
+                <Grid item md={5} className="logoBox">
+                    <img src={logo} alt="logo"/>
+                    <Typography variant='h1'>Welcome Back!</Typography>
+                    <Typography variant='h5'>login account</Typography>
+                    <Box className='form-box'>
+                        <Grid container>
+                            <Grid className='flagBox' item xs={12} sm={3}>
+                            <img className='flag-img' src={flag} alt="flag"/>
+                                <select> 
+                                    <option> +91</option>
+                                    <option> +92</option>
+                                    <option> +93</option>
+                                    <option> +94</option>
+                                </select>
+                            </Grid>
+                            <Grid className='inputbox' item xs={12} sm={9}>
+                                <input className='inputfield' placeholder="Mobile no" />
+                                <img src={phone_icon} alt="phone"/>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    <Grid>
+                        <Button id='mybtn'>Get OTP</Button>
+                    </Grid>
+                </Grid>
+                <Grid item md={7} className="bannerBox">
+                    <img src={login_banner} alt="banner"/>
+                </Grid>
+            </Grid>
+        </Container>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@500;600;700&family=Montserrat+Alternates:wght@500;600&display=swap');
+</style>;
+<link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@500;600;700&display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@500;600;700&family=Montserrat+Alternates:wght@500;600&display=swap" rel="stylesheet"></link>
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
